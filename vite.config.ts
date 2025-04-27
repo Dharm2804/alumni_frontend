b90204c0b79bd48e70f3a66a60805cb70af6381e
Dharm2804 <kasundradharm2845@gmail.com>
2025-04-27 21:37:48 +0530

    cors change

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://alumni-backend-6xcz.onrender.com',
         changeOrigin: true,
       },
     },
